ae2b7c7075bdee0326a9746080fd071735aa2949
Arturo Yunior Torres Torre <arturo.torres.t@uni.pe>
2026-01-28 18:04:44 -0500

    Pude hacerlo 98% bien
    solo necesito arreglar unos pequeños errores,
    ,
    Gracias por proponernos este problema profesor. APrendí muchísimo con él

---
+++ (semana3) TEMA4.2 ARR/FastCOPY.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+//Función1
+
+
+//Función2
+
+
+//Función3
+
+
+
+//Función4
+
+
+
+//Función5
+
+
+
+//Función6
+
+int main(){
+
+
+
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+cd ~/Pictures/"(semana3) TEMA4.2 ARR"
+
+make TEMA4.2_1Ejer_2doINTENTO
+./TEMA4.2_1Ejer_2doINTENTO
