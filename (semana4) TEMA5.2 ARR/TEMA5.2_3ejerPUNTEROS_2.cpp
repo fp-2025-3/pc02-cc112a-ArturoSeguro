6a74b6acc2847303d10f3136b6b31ba9d1264505
Arturo Yunior Torres Torre <arturo.torres.t@uni.pe>
2026-02-05 16:37:23 -0500

    -> (ejer2) 1erAvance
    no es necesario crear subArrays

--- (semana4) TEMA5.2 ARR/TEMA5.2_3ejerPUNTEROS_2.cpp
+++ (semana4) TEMA5.2 ARR/TEMA5.2_3ejerPUNTEROS_2.cpp
@@ -1,19 +1,30 @@
 #include <iostream>
+#include <ctime>        //time(0)
+#include <cstdlib>      //srand()   
 using namespace std;
 
-//function1
+//function1 --> condición(1)
+bool alMenosUno(un array, un indice de inicio){  //seguro le meteré un SUBarray extraido del original (A)
+
+}
 
 
-//function2
+//function2 --> operación(2) --> resultado de cada segmento
+int resultadoSegmento(un array, un indice de inicio){    //a este le meteré un SUBarray extraido del original (A) también seguro
+
+}
 
 
 //function3
 
 
+
 //function4
 
 
+
 //function5
+
 
 
 //function6
@@ -21,13 +32,48 @@ using namespace std;
 
 
 
+
+
+
 //main
 //
 int main(){
+    //1. creando arreglo A
+    int A[15];
+    srand(time(0));
+
+    for(int i=0; i<15; i++){
+        A[i]= rand()%20;   //de (0-19)
+    }
+
+
+
+
+    //function1
+
+
+
+    //function2
+
+
+
+    //function3
+
+
+
+    //function4
+
+
+
+    //function5
+
+
+
+    //function6
 
     
 
-
+    //
     cout << "\n\n-------------END\n";
     return 0;
 }
