bee29085ef192ab42650e2f7cc003f8e255cc736
Arturo Yunior Torres Torre <arturo.torres.t@uni.pe>
2026-02-05 17:01:34 -0500

    -> (ejer2) 2do avance
    termine de esquematizar mi problema

--- (semana4) TEMA5.2 ARR/TEMA5.2_3ejerPUNTEROS_2.cpp
+++ (semana4) TEMA5.2 ARR/TEMA5.2_3ejerPUNTEROS_2.cpp
@@ -4,30 +4,35 @@
 using namespace std;
 
 //function1 --> condición(1)
-bool alMenosUno(un puntero al inicio del array, un indice de inicio){  //no es necesario crear un SubArray, como si fuera un MergeSort, puedo moverme por la función original con puros punteros
+bool alMenosUno(un puntero al inicio del arrayOriginal, un indice de inicio, un indice de final){  //no es necesario crear un SubArray, como si fuera un MergeSort, puedo moverme por la función original con puros punteros
 
 }
+
+
 
 
 //function2 --> operación(2) --> resultado de cada segmento
-int resultadoSegmento(un puntero al inicio del array, un indice de inicio){
+int resultadoSegmento(un puntero al inicio del arrayOrignial, un indice de inicio, un indice de final){ //como lo usaré recursivamente, pues el inicio y final cmabiarán en diversos casos
+//                                                                                                       La eficiencia del programa dependerá del tipo de condiciones que le agregue:: el arbol/ramificación producto de la recursividad debe ser lo más eficiente posíble
+}
+
+
+
+
+
+//function3 --> operación(3) --> resultado de costo total (sumando todas los valores de segmento)
+int costoTotal(un array de punteros que contenga todos los inicios de los segmentos creados, un puntero al inicio del array que contenga las longitudes de cada Segmento que forme){
 
 }
 
 
-//function3
 
 
 
-//function4
+//function4 --> función recursica(4) --> aplicará de manera adecuada y con condiciones adecuadas las anteriores funciones 
+int exploradorDePosibilidades(un puntero al inicio del arrayOriginal, un indice de inicio, un indice de final){
 
-
-
-//function5
-
-
-
-//function6
+}
 
 
 
@@ -35,7 +40,11 @@ int resultadoSegmento(un puntero al inicio del array, un indice de inicio){
 
 
 
-//main
+
+
+//main---------------------------------------
+//
+//
 //
 int main(){
     //1. creando arreglo A
@@ -43,35 +52,16 @@ int main(){
     srand(time(0));
 
     for(int i=0; i<15; i++){
-        A[i]= rand()%20;   //de (0-19)
+        A[i]= (rand()%20)-2;   //de (-2 a 17) --> (los negativos harán el ejercicio más interesante)
     }
 
 
 
-
-    //function1
-
-
-
-    //function2
+    //2. LLamo a "function4" (el cual llama a las demás convenientemente) y deteermino la solución
+    int costoMinimoPosible= exploradorDePosibilidades(&A[0], 0, 14);
+    cout << "El costo minimo posible es:: " << costoMinimoPosible;
 
 
-
-    //function3
-
-
-
-    //function4
-
-
-
-    //function5
-
-
-
-    //function6
-
-    
 
     //
     cout << "\n\n-------------END\n";
