0961bf9ac43440790176c83ddbd737b8040ee653
Arturo Yunior Torres Torre <arturo.torres.t@uni.pe>
2026-02-06 11:17:50 -0500

    -> (ejer3) completado con las correcciones finales
    es el programa más hermoso que he construido en mi vida.
    Esta muy bueno y ordenado.
    .
    Me costo muchas horas pero aprendí mucho sobre el orden (mi estilo de orden) que ayuda a trabajar más rapido

--- (semana4) TEMA5.2 ARR/TEMA5.2_3ejerORDENAMIENTO_3.cpp
+++ (semana4) TEMA5.2 ARR/TEMA5.2_3ejerORDENAMIENTO_3.cpp
@@ -8,14 +8,14 @@ using namespace std;
                     void generadorCodigos(int* codigos, int N){
                         for(int i=0; i<N; i++){
                             //
-                            codigos[i]= (2025*1000)+ (rand()%1000);
+                            codigos[i]= (2025*1000)+ (rand()%1000);     
 
                             //
                             int j=0;
                             while(j<i){
-                                bool condicion= codigos[i]!= codigos[j];
+                                bool condicion= codigos[i]== codigos[j];    //(error3) quiero analizar si son iguales y ahí recién hacer el cambio;
                                 if(condicion){
-                                    i--;    //hará que se vuelva a generar un numALEA por si se repitio
+                                    i--;    
                                     break;
                                 }
                                 j++;
@@ -27,10 +27,10 @@ using namespace std;
                     }
 
                     //2. Notas
-                    void generadorNotas(int* notas, int N){
+                    void generadorNotas(float* notas, int N){
                         for(int i=0; i<N; i++){
                             //
-                            notas[i]= (rand()%21)+1;
+                            notas[i]= (rand()%21);  //(error4) el "+1" no es lo que quiero --> con eso podría obtener "21" de nota
                         }
 
                         //
@@ -52,36 +52,36 @@ using namespace std;
 
 //Ordenando por notas ((usaré mergeSort para practicar bien bien))---------
         //4. Merge
-        void merge(int* notas, int l, int mid, int r, int* codigos, int* inasistencias){
+        void merge(float* notas, int l, int mid, int r, int* codigos, int* inasistencias){
             //tamaño de sub-arreglos
-            int tamL= (mid-l)+ 1;       //AA..                 //el "+1" es importante ya que uno de los dos lados es el que se tiene que quedar con uno más cuando la cantidad de elemtos es impar
-            int tamR= (r-mid);          //BB..
+            int tamL= (mid-l)+ 1;      
+            int tamR= (r-mid);          
 
             //arreglos temporales (heap)
-            int *notasL= new int[tamL];     //AA..
-            int *notasR= new int[tamR];     //BB..
+            float *notasL= new float[tamL];    
+            float *notasR= new float[tamR];  
             //
-            int *codigosL= new int[tamL];     //AA..
-            int *codigosR= new int[tamR];     //BB..
+            int *codigosL= new int[tamL];   
+            int *codigosR= new int[tamR];  
             //
-            int *inasistenciasL= new int[tamL];     //AA..
-            int *inasistenciasR= new int[tamR];     //BB..
+            int *inasistenciasL= new int[tamL];   
+            int *inasistenciasR= new int[tamR];    
 
 
             //los convierto en lo que quiero (las mitades correspondientes del arreglo notas[] --> que también está en el "heap")
             for(int i=0; i<tamL; i++){
-                notasL[i]= notas[l+i];          //AA..
+                notasL[i]= notas[l+i];        
                 //
                 codigosL[i]= codigos[l+i];
                 //
                 inasistenciasL[i]= inasistencias[l+i];
             }
             for(int i=0; i<tamR; i++){
-                notasR[i]= notas[(mid+1)+i];    //BB..          //este "+1" es distinto al del comienzo,,,, este "+1" dice que se tiene que declarar uno más adelante por que el "A..." era el que tenia más (primer "1") elementos y por ende tú (B..) tienes que irte uno más adelante para recien comenzarte a añadir elementos 
+                notasR[i]= notas[(mid+1)+i];    
                 //
-                codigosL[i]= codigos[(mid+1)+i];
+                codigosR[i]= codigos[(mid+1)+i];
                 //
-                inasistenciasL[i]= inasistencias[(mid+1)+i];
+                inasistenciasR[i]= inasistencias[(mid+1)+i];
             }
 
 
@@ -91,8 +91,8 @@ using namespace std;
             int inCombinado= l;
 
             //el menor de las 2 mitades
-            while(inL< tamL && inR< tamR){
-                if(notasL[inL]< notasR[inR]){
+            while(inL< tamL && inR< tamR){                              //(ERROR1) lo había hecho de menor -> mayor ((Se pedi ade mayor -> menor))
+                if(notasL[inL]== notasR[inR]){
                     if(codigosL[inL]< codigosR[inR]){
                         notas[inCombinado]= notasL[inL];
                         //
@@ -108,7 +108,7 @@ using namespace std;
                         inasistencias[inCombinado]= inasistenciasR[inR];
                         inR++;
                     }
-                }else if(notasL[inL]< notasR[inR]){
+                }else if(notasL[inL]> notasR[inR]){
                     notas[inCombinado]= notasL[inL];
                     //
                     codigos[inCombinado]= codigosL[inL];
@@ -140,7 +140,7 @@ using namespace std;
             }
 
             //el resto de "R" si llegan a sobrar
-            while(inL< tamR){
+            while(inR< tamR){                                   //(error5) pequeños errores que me cuestan muy caro luego
                 notas[inCombinado]= notasR[inR];
                 //
                 codigos[inCombinado]= codigosR[inR];
@@ -163,7 +163,7 @@ using namespace std;
         }
 
         //5. MergeSort
-        void mergeSort(int* notas, int l, int r, int* codigos, int* inasistencias){
+        void mergeSort(float* notas, int l, int r, int* codigos, int* inasistencias){
             //BASE
             if(l>= r){
                 return;
@@ -179,7 +179,7 @@ using namespace std;
             mergeSort(notas, mid+1, r, codigos, inasistencias);
 
             //ordeno las 2mitades YA ORDENADAS
-            merge(notas, l, mid, r, codigos, inasistencias);            //el primero que se llame aca, será el último en resolverse (En el esquema de recursividad)
+            merge(notas, l, mid, r, codigos, inasistencias); 
         }
 
 
@@ -191,17 +191,17 @@ using namespace std;
 
 //Ranking académico---------
                     //6. RankingAcadémico
-                    void ranking(int* notas, int* codigos, int N){
+                    void ranking(float* notas, int* codigos, int N){
                         //top 5
                         cout << "Lista de los 5 mejores:: \n";
-                        cout << "CODIGOS        PROMEDIOS       PUESTO\n";
+                        cout << "CODIGOS\t\tPROMEDIOS\tPUESTO\n";           //los "\t" me ofrecen más orden
                         for(int i=0; i<5; i++){
-                            cout << codigos[i] << "       " << notas[i] << "         " << i+1;
+                            cout << codigos[i] << "\t\t" << notas[i] << "\t\t" << i+1;
                             cout << "\n";
                         }
 
                         //promedio del curso
-                        int notasTotal=0;
+                        float notasTotal=0;
                         for(int i=0; i<N; i++){
                             notasTotal= notasTotal+ notas[i];
                         }
@@ -224,23 +224,23 @@ using namespace std;
 
 //Busqueda---------
         //7. BusquedaLineal
-        void lineal(int* notas, int* codigos, int* inasistencias, int N, int codigoBuscado){
+        void lineal(float* notas, int* codigos, int* inasistencias, int N, int codigoBuscado){
 
             int rankingPlace= -1;
             for(int i=0; i<N; i++){
                 if(codigos[i]== codigoBuscado){
-                    int rankingPlace= i+1;
+                    rankingPlace= i+1;              //(ERROR2) el "int" de adentro malograba todo
                     break;
                 }
             }
 
-            int j=rankingPlace-1;               //para mantenerlo simple mi codigo
+            int j=rankingPlace-1;
             if(rankingPlace== -1){
                 cout << "no existe el codigo introducido.\n";
                 return;             
             }else{
-                cout << "NOTAS      FALTAS      PUESTO\n";
-                cout << notas[j] << "       " << inasistencias[j] << "       " << rankingPlace;
+                cout << "NOTAS\tFALTAS\tPUESTO\n";
+                cout << notas[j] << "\t" << inasistencias[j] << "\t" << rankingPlace;
             }
 
             return;
@@ -249,17 +249,23 @@ using namespace std;
 
 //2do Ordenamiento---------
                     //8. InsertionSort
-                    void insertion(int* codigos, int* notas, int* inasistencias, int N){
+                    void insertion(int* codigos, float* notas, int* inasistencias, int N){      //(ERROR6) no lo programe adecuadamente
                         for(int i=1; i<N; i++){
-                            for(int j=i-1; j>=0; j--){      
-                                if(codigos[i]< codigos[j]){
-                                    swap(codigos[i], codigos[j]);
+                            int j= i-1;
+                            while(j>= 0 && codigos[j]> codigos[i]){
+                                codigos[j+1]= codigos[j];
+                                //paralelamente
+                                notas[j+1]= notas[j];
+                                inasistencias[j+1]= inasistencias[j];
 
-                                    //paralelamente
-                                    swap(notas[i], notas[j]);
-                                    swap(inasistencias[i], inasistencias[j]);
-                                }
+                                j--;    
                             }
+
+                            codigos[j+1]= codigos[i];
+                            //paralelo
+                            notas[j+1]= notas[i];
+                            inasistencias[j+1]= inasistencias[i];
+
                         }
 
                         return;
@@ -269,23 +275,23 @@ using namespace std;
 
 //2da Busqueda---------
         //9. busquedaBinaria
-        int binaria(int* codigos, int N, int l, int r, int clave){  //retornará el indice en donde está el resultado
+        int binaria(int* codigos, int N, int l, int r, int clave){
             //Base
             if(l>r){
-                return -1;      //significa que no encontró lo que se deseaba
+                return -1;
             }
 
             //
             int mitad= (l+r)/2;
-            if(mitad== clave){
+            if(codigos[mitad]== clave){      //(error7) no puedo comparar un indice con un elemento del array
                 return mitad;
-            }else if(clave<mitad){
+            }else if(clave< codigos[mitad]){
                 return binaria(codigos, N, l, mitad-1, clave);
             }else{
                 return binaria(codigos, N, mitad+1, r, clave);
             }
 
-            return -2;      //si algo falla me retornará "-2"
+            //no es necesario otro "return" (tiene que caer en una de mis opciones ya programadas)
         }
 
 
@@ -303,7 +309,7 @@ int main(){
     srand(time(0));
 
     int N;
-    cout << "Ponga el numero de alumnos que hay:: ";
+    cout << "Ponga el numero de alumnos que hay (mayor a 5):: "; //(error9) es necesario ">5"para no tener errores de overflow en la función "ranking()""
     cin >> N;
 
     //1.GeneroCódigos
@@ -311,7 +317,7 @@ int main(){
     generadorCodigos(codigos, N);
 
     //2.GeneroNotas
-    int* notas= new int[N];
+    float* notas= new float[N];             //(error8) los cambie todas las notas[] a float porque así me darán decimales correcta en "el promedio del curso"
     generadorNotas(notas, N);
 
     //3.GeneroInasistencias
@@ -321,7 +327,7 @@ int main(){
 
 
 
-    cout << "\n\nODENADO por NOTA------------------------------\n";
+    cout << "\n\nODENADO por NOTA (descendente)------------------------------\n";
     //4y5.Ordenando por notas y manteniendo relación
     mergeSort(notas, 0, N-1, codigos, inasistencias);
 
@@ -330,7 +336,7 @@ int main(){
 
     //
     int codigoBuscado;
-    cout << "Escriba el codigo que desea buscar:: ";
+    cout << "\nEscriba el codigo que desea buscar:: ";
     cin >> codigoBuscado;
     //7.BusquedaLineal
     lineal(notas, codigos, inasistencias, N, codigoBuscado);
@@ -338,7 +344,7 @@ int main(){
 
 
 
-    cout << "\n\nODENADO por CODIGO------------------------------\n";
+    cout << "\n\nODENADO por CODIGO (ascendente)------------------------------\n";
     //8.SegundoOrdenamiento (Insertion)
     insertion(codigos, notas, inasistencias, N);
 
@@ -348,15 +354,12 @@ int main(){
     cin >> codigoBuscado2;
     //9.BusquedaBinaria
     int inEncontrado= binaria(codigos, N, 0, N-1, codigoBuscado2);
-    bool condicion1= (inEncontrado== -1);
-    bool condicion2= (inEncontrado== -2);
-    if(condicion1){
+    if(inEncontrado== -1){
         cout << "\nNo se encontro el numero (asegurese de que existe)\n";
-    }else if(condicion2){
-        cout << "Algo fallo en la busqueda\n";
     }else{
-        cout << "NOTAS      FALTAS      PUESTO\n";
-        cout << notas[inEncontrado] << "       " << inasistencias[inEncontrado] << "        " << inEncontrado+1;
+        cout << "NOTAS\tFALTAS\tPUESTO\n";
+        cout << notas[inEncontrado] << "\t" << inasistencias[inEncontrado] << "\t" << inEncontrado+1;
+        cout << "\n";
     }
 
 
