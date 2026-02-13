b9e7822a42f22ec35cd2a1caded74e5683a5b55a
Arturo Yunior Torres Torre <arturo.torres.t@uni.pe>
2026-02-13 12:21:09 -0500

    -> (4) 1st version

--- (semana5) TEMA6.2 ARR/TEMA6.2_4EXERCISE.cpp
+++ (semana5) TEMA6.2 ARR/TEMA6.2_4EXERCISE.cpp
@@ -12,63 +12,52 @@ struct polinomial{
 
 //filling (function2)
 polinomial* filling(int degree){
-    polinomial* head= nullptr;          //pointer prepared to point a variable of the type y made in the struct
-    polinomial* last= nullptr;          //pointer prepared to point a variable of the type y made in the struct  
+    polinomial* head= nullptr;
+    polinomial* last= nullptr;
 
-    for(int i=degree; i>= 0; i--){                  //it is necessary to include the "0" too.             
-        polinomial* ActualNode= new polinomial;
-        (*ActualNode).next= nullptr;            //the intern pointer of this Actual(new) Structure during this loop will indicate to nothing --> in the next loop it is gonna to pint the struct of that same next loop
+    for(int i=degree; i> 0; i--){                                   //((ERROR1)) I THINK IT IS WRONG. I SHOULDN´T USE [i]
+        polinomial* newNode= new polinomial;
 
-        (*ActualNode).degree= i;
-        cout << "Input the coefficient of 'x^" << i << "':: ";
-        cin >> (*ActualNode).coefficient;
-    
-        //Inserting in the list
+        cout << "put the coefficient of 'x^" << i << "':: ";
+        cin >> (*newNode).degree;
+
+
+        (*newNode).next= nullptr;
+
+        //logic
         if(head== nullptr){
-            //if list is the first node, it is both head and last
-            head= ActualNode;
-            last= ActualNode;
+            //if it is the first node, it is both
+            head= newNode;
+            last= newNode;
         }else{
-            //Attach "this actual newNode" to the end of the list (first)
-            (*last).next= ActualNode;      //changing pointers of structures (the intern(inside) pointer of the previos structure will point to this new one on this loop)
-            last= ActualNode;              //changing pointers of structures (and the pointer outside this system of structures will point always to the actual strcuture  -->--> jus in the previous line "last" was the precious structure, but in this new line, no more)
+            (*last).next= newNode;
         }
+        
+
     }
 
-    return head;        
 }
 
 
 //Showing POlinomials (function3)
-void printingPolinomial(polinomial* head){
-    polinomial* current1= head;                  //it's better term it as "current" then "actual"
-
-    while(current1!= nullptr){
-        cout << (*current1).coefficient << "x^" << (*current1).degree;
-
-        //analyzing whether this pointer structure has a structure pointer pointing to something or "nullptr" or not (if not that means that there is no more --> I'm in the last one, so i don't have to put a "+")
-        if( (*current1).next!= nullptr){
+void printingPolinomial(polinomial* actualP, int degree){
+    polinomial* head2= nullptr;
+    for(int i=degree; i> 0; i++){
+        if(i== degree){
+            //nothing
+        }else{
             cout << " + ";
         }
-        current1= (*current1).next;
-    }
+        cout << "x^" << actualP[i].coefficient;
 
-    cout << "\n";
-    return;
+
+
+    }
 }
 
 
-//Deleting hype variables
-void deleteList(polinomial* head){
-    while(head!= nullptr){
-        polinomial* current2= head;
-        head= (*head).next;             //going in the structure inside the currentSTRUCTURE (of this loop)
+//function4
 
-        delete current2;
-    }
-
-    return;
-}
 
 //function5
 
@@ -83,41 +72,26 @@ void deleteList(polinomial* head){
 //
 //
 int main(){
-    int N;
-    cout << "Provide the number of polinomials:: ";
-    cin >> N;
-    //the DOUBLE pointer is pretty necessary to create an array od Structure pointers
-    polinomial** POINpolinonial= new polinomial*[N];      //this ARRAYpointer  --> points --> to an array of Actual Structures, not Structures pointers
+    polinomial* POINpolinonial= new polinomial[2];
 
-    cout << "\n";
     //(function2)
-    for(int i=0; i<N; i++){
-        cout << "\n\n";
-        int tempDegree;
-        cout << i+1 << "___ Provide the degree of the polinomial:: ";
-        cin >> tempDegree;
-        POINpolinonial[i]= filling(tempDegree);
+    for(int i=0; i<2; i++){
+        cout << "Provide the degree of the polinomial:: ";
+        cin >> POINpolinonial[i].degree;
+        *POINpolinonial= filling(POINpolinonial+i, POINpolinonial[i].degree);
 
         cout << "\n\n";
     }
 
 
-    //RESULTSSSSSSSSSSSSSS-------------
     //showing polinomials
-    for(int i=0; i<N; i++){
-        cout << i+1 << "___ Polinomial:: ";
-        printingPolinomial(POINpolinonial[i]);
-    }
-
-
-    //showing degress (NOW I CAN with the sintax i was trying before)
-    cout << "\n";
-    for(int i=0; i<N; i++){
-        cout << i+1 << "___ Polinomial degree:: ";
-        cout << POINpolinonial[i][0].degree;
+    for(int i=0; i<2; i++){
+        cout << i+1 << " Polinomial:: ";
+        printingPolinomial(POINpolinonial+i, POINpolinonial[i].degree);
 
         cout << "\n";
     }
+
 
     //function1
 
@@ -139,10 +113,6 @@ int main(){
 
 
     //Deleting
-    for(int i=0; i<N; i++){
-        deleteList(POINpolinonial[i]);
-    }
-
 
     delete[] POINpolinonial;
     
