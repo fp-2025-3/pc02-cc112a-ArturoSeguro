6afc9f3b8da2078a9b9b4e560a3fa7cd56cc9e5e
Arturo Yunior Torres Torre <arturo.torres.t@uni.pe>
2026-02-14 21:41:12 -0500

    (ejer3-PC4) corregido el sistema de punteros

--- (semana5) TEMA6.3 ARR/3ejer_arr.cpp
+++ (semana5) TEMA6.3 ARR/3ejer_arr.cpp
@@ -2,9 +2,10 @@
 using namespace std;
 
 
+//--1
 //
 struct producto{
-    char* nombre;               //cadea dinamica
+    char* nombre;
 
     int codigo;
     double precio;
@@ -12,140 +13,100 @@ struct producto{
 };
 
 
+//--2
 //
-//1
-producto crearProducto(){
-    //STRUCT QUE SE DEVOLVERÁ
-    producto actualPRO;
-
+producto* crearProducto(){
+    producto* actualPRO= new producto;
 
     //
     char* PUNnombre= new char[20];
     cout << "\n\n\nColoque el nombre:: ";
     cin.getline(PUNnombre, 20);
-    actualPRO.nombre= PUNnombre;    
-
+    (*actualPRO).nombre= PUNnombre;    
     //
-    cout << "\nColoque el codigo:: ";
-    cin >> actualPRO.codigo;
+    cout << "Coloque el codigo:: ";
+    cin >> (*actualPRO).codigo;
     cin.ignore();
-
     //
-    cout << "\nColoque el precio:: ";
-    cin >> actualPRO.precio;
+    cout << "Coloque el precio:: ";
+    cin >> (*actualPRO).precio;
     cin.ignore();
-
     //
-    cout << "\nColoque el stock:: ";
-    cin >> actualPRO.stock;
+    cout << "Coloque el stock:: ";
+    cin >> (*actualPRO).stock;
     cin.ignore();
 
     return actualPRO;
 }
 
 
-//3. INVENTARIO
+//--3. INVENTARIO
+//
 void inventario(producto* actualPRO2){
-    cout << "COdigo: " << (*actualPRO2).codigo << "\t|| NOmbre: " << (*actualPRO2).nombre << 
+    cout << "Codigo: " << (*actualPRO2).codigo << "\t|| Nombre: " << (*actualPRO2).nombre << 
     "\t|| Precio: " << (*actualPRO2).precio << "\t|| Stock: " << (*actualPRO2).stock << "\n";
 }
 
 
-
-
-
-
-
-
-//2
-producto* buscarProducto(producto* PRO, int cantidad, int codigoBuscado){
+//--4
+//
+producto* buscarProducto(producto** PRO, int cantidad, int codigoBuscado){
     for(int i=0; i<cantidad; i++){
-        if(PRO[i].codigo== codigoBuscado){
-            return PRO+i;
+        if((*PRO[i]).codigo== codigoBuscado){
+            return PRO[i];
         }
     }
 
-    return nullptr;     //no existe producto
+    return nullptr;
 }
 
 
-//function4
-
-
-//function5
-
-
-//function6
-
-
-
-
 //main
-//
-//
 //
 int main(){
     int cantidad;
     cout << "\nColoque el numero de productos a registrar: ";
     cin >> cantidad;
     cin.ignore();
+    producto** PRO= new producto*[cantidad];
 
-    //
-    producto PRO[cantidad];
 
-    //1
+    //--2
     cout << "\n";
     for(int i=0; i<cantidad; i++){
         PRO[i]= crearProducto();
     }
 
 
-
-
-    //3. Inventario
+    //--3
+    cout << "\n\n\nINVENTARIO----------\n";
     for(int i=0; i<cantidad; i++){
-        producto* TEMPpro= &PRO[i];
+        producto* TEMPpro= PRO[i];
         inventario(TEMPpro);
     }
 
 
-
-
-
-
-
-
-
-    //2 ((pidiendo producto))
+    //--4
     int codigoBuscado;
     cout << "\n\n\nCOloque el codigo del producto que busca:: ";
     cin >> codigoBuscado;
     cin.ignore();
-
+    //
     producto* productoBuscado= buscarProducto(PRO, cantidad, codigoBuscado);
-    cout << "\nBuscando el producto de codigo " << codigoBuscado;
+    cout << "\nBuscando el producto de codigo " << codigoBuscado << "........";
     if(productoBuscado== nullptr){
         cout << "\nProducto no encontrado";
     }else{
         cout << "\nProducto encontrado:: " << (*productoBuscado).nombre << " | " << "Precio: " << (*productoBuscado).precio;
     }
  
-    //function1
 
-
-    //function2
-
-
-    //function3
-
-
-    //function4
-
-
-    //function5
-
-
-    //function6
+    //delete
+    for(int i=0; i<cantidad; i++){
+        delete[] (*PRO[i]).nombre;  //deleting (char) lo creado en "crearProducto()""
+        delete PRO[i];              //deleting (struct) lo creado también en "crearProducto()"
+    }
+    delete[] PRO;                   //deleting (struct) lo creado en este main (**PRO)
     
 
 
