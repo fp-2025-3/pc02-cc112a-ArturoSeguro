54f7b1ad312ee4a07749bbf0f7568b47a3c6859b
Arturo Yunior Torres Torre <arturo.torres.t@uni.pe>
2026-02-14 21:06:41 -0500

    -> (ejer2-PC4) correción con tabla bien impresa
    eleementos de "struct team{}" ordenados

--- (semana5) TEMA6.3 ARR/2ejer_arr.cpp
+++ (semana5) TEMA6.3 ARR/2ejer_arr.cpp
@@ -3,49 +3,30 @@
 #include <ctime>
 using namespace std;
 
-//
-struct goals{
-    int favor=0;
-    int contra=0;
-};
- 
-//
-struct match{
-    float empatado;
-    float perdido;
-    float ganado;
-};
+                //--2
+                struct goals{
+                    int favor=0;
+                    int contra=0;
+                };
+                
+                //--1
+                struct match{
+                    int empatado;
+                    int perdido;
+                    int ganado;
+                };
 
-//PRINCIPAL
+//--0.PRINCIPAL
 struct team{
     string nombres;
     float puntos;  
 
-    match partidos;
-    goals goles;
+    match partidos;     //--1
+    goals goles;        //--2
 
     int diferenciaGoles;
     float performance;
 };
-
-//function1
-
-
-//function2
-
-
-//function3
-
-
-//function4
-
-
-//function5
-
-
-//function6
-
-
 
 
 //main
@@ -53,137 +34,127 @@ struct team{
 //
 //
 int main(){
-
-    //
-    int cantidad= 5;
     srand(time(0));
-
-    //
+    int cantidad= 5;
     team* PUNpaises= new team[cantidad];
 
-    //PARTIDOOOOOOOOOOOOOOOOS
+
+    //i) PARTIDOOOOOOOOOOOOOOOOS--
     for(int i=0; i<cantidad; i++){
         //
         cout << "Dea el nombre del equipo: ";
         getline(cin, PUNpaises[i].nombres);
 
-        //
+        //Esta forma de generar aleatoria es mucho más simple
         PUNpaises[i].partidos.ganado= rand()%101;
-
-        //
-        int actualGana= PUNpaises[i].partidos.ganado;
-        if(actualGana< 100){
-            PUNpaises[i].partidos.empatado= rand()%(101- actualGana);
-        }else{
-            PUNpaises[i].partidos.empatado= 0;
-            PUNpaises[i].partidos.perdido= 0;
-        }
-
-        //
-        int actualEmpata= PUNpaises[i].partidos.empatado;
-        if(actualGana+actualEmpata< 100){
-            PUNpaises[i].partidos.perdido= rand()%(101- actualGana-actualEmpata);
-        }else{
-            PUNpaises[i].partidos.perdido= 0;
-        }
+        PUNpaises[i].partidos.empatado= rand()%(101- PUNpaises[i].partidos.ganado);
+        PUNpaises[i].partidos.perdido= 100- PUNpaises[i].partidos.ganado- PUNpaises[i].partidos.empatado;
     }
 
-    //impresion sin goles
-    cout << "PAIS\t\t\t\tGANADO\t\tPERDIDO\t\tEMPATADO\t\tPUNTOS\t\tPERFORMANCE\n";
-    for(int i=0; i<cantidad; i++){
-        cout << "Pais " << i << "\t\t" << PUNpaises[i].partidos.ganado << "\t\t" 
-        << PUNpaises[i].partidos.perdido << "\t\t" << PUNpaises[i].partidos.empatado << "\t\t"
-        << PUNpaises[i].puntos << "\t\t" << PUNpaises[i].performance << "\n";
-    }
 
-    //GOLEEEEEEEEEEEEEEEES
-    //ganados
+                    //ii) GOLEEEEEEEEEEEEEEEES--
+                    //ganados
+                    for(int i=0; i<5; i++){
+                        for(int j=0; j<PUNpaises[i].partidos.ganado; j++){
+                            int TEMPfavor= (rand()%5)+1;        //entre 1-5 para luego usar correctamente el siguiente rand()
+                            int TEMPcontra= rand()%TEMPfavor;   //este rand() daría error si fuer --> rnad()%0 --> ERROR --> ya no sucederá
+
+                            PUNpaises[i].goles.favor= 
+                            PUNpaises[i].goles.favor+ TEMPfavor;
+                            PUNpaises[i].goles.contra= 
+                            PUNpaises[i].goles.contra+ TEMPcontra;
+                        }
+                    }
+                    //perdidos
+                    for(int i=0; i<5; i++){
+                        for(int j=0; j<PUNpaises[i].partidos.perdido; j++){
+                            int TEMPcontra= (rand()%5)+1;
+                            int TEMPfavor= rand()%TEMPcontra;
+
+                            PUNpaises[i].goles.favor= 
+                            PUNpaises[i].goles.favor+ TEMPfavor;
+                            PUNpaises[i].goles.contra= 
+                            PUNpaises[i].goles.contra+ TEMPcontra;
+                        }
+                    }
+                    //empatados
+                    for(int i=0; i<5; i++){
+                        for(int j=0; j<PUNpaises[i].partidos.empatado; j++){
+                            int TEMPcontra= (rand()%5)+1;
+                            int TEMPfavor= TEMPcontra;
+
+                            PUNpaises[i].goles.favor= 
+                            PUNpaises[i].goles.favor+ TEMPfavor;
+                            PUNpaises[i].goles.contra= 
+                            PUNpaises[i].goles.contra+ TEMPcontra;
+                        }
+                    }
+
+
+    //iii) DIFERENCIA GOLES & PUNTOS & PERFORMANCE
     for(int i=0; i<5; i++){
-        for(int j=0; j<PUNpaises[i].partidos.ganado; j++){
-            int TEMPfavor= rand()%6;
-            int TEMPcontra= rand()%TEMPfavor;
+        //DG
+        PUNpaises[i].diferenciaGoles=
+        PUNpaises[i].goles.favor- PUNpaises[i].goles.contra;
 
-            PUNpaises[i].goles.favor= PUNpaises[i].goles.favor+ TEMPfavor;
-            PUNpaises[i].goles.contra= PUNpaises[i].goles.contra+ TEMPcontra;
-        }
-    }
-
-    //perdidos
-    for(int i=0; i<5; i++){
-        for(int j=0; j<PUNpaises[i].partidos.perdido; j++){
-            int TEMPcontra= rand()%6;
-            int TEMPfavor= rand()%TEMPcontra;
-
-            PUNpaises[i].goles.favor= PUNpaises[i].goles.favor+ TEMPfavor;
-            PUNpaises[i].goles.contra= PUNpaises[i].goles.contra+ TEMPcontra;
-        }
-    }
-
-    //perdidos
-    for(int i=0; i<5; i++){
-        for(int j=0; j<PUNpaises[i].partidos.empatado; j++){
-            int TEMPcontra= rand()%6;
-            int TEMPfavor= TEMPcontra;
-
-            PUNpaises[i].goles.favor= PUNpaises[i].goles.favor+ TEMPfavor;
-            PUNpaises[i].goles.contra= PUNpaises[i].goles.contra+ TEMPcontra;
-        }
-    }
-
-    //GOLES DIFERENCIA
-    for(int i=0; i<5; i++){
-        PUNpaises[i].diferenciaGoles= PUNpaises[i].goles.favor- PUNpaises[i].goles.contra;
-    }
-
-
-
-
-
-    //TABLAAAAAAAAAAAAAAAAAAA
-    cout << "PAIS\t\t\tGANADO\t\tPERDIDO\t\tEMPATADO\t\tGOLES.FAVOR\t\tGOLES.CONTRA\t\tGOLES.DIFERENCIA\t\tPUNTOS\t\tPERFORMANCE\n";
-    for(int i=0; i<cantidad; i++){
-        cout << "Pais " << i << "\t\t" << PUNpaises[i].partidos.ganado << "\t\t" 
-        << PUNpaises[i].partidos.perdido << "\t\t" << PUNpaises[i].partidos.empatado << "\t\t" 
-        << PUNpaises[i].goles.favor << "\t\t" << PUNpaises[i].goles.contra << "\t\t" << PUNpaises[i].diferenciaGoles << "\t\t"
-        << PUNpaises[i].puntos << "\t\t" << PUNpaises[i].performance << "\n";
-    }
-
-
-    //el mayor funciónss
-    int performanceMayor= -1;
-    int INpais= -1;
-    for(int i=0; i<cantidad; i++){
-        if(performanceMayor< PUNpaises[i].performance){
-            performanceMayor= PUNpaises[i].performance;
-            INpais= i;
-        }
-    }
-
-    cout << "\n\nCAMPEON: Pais_ << " << INpais << " | PUNTOS: " << PUNpaises[INpais].puntos  << " | GOLES.DIFERENCIA: " << PUNpaises[INpais].diferenciaGoles  << " | PERFORMANCE: " << PUNpaises[INpais].performance << "%";
-
-
-
-
-
-    //function1
-
-
-    //function2
-
-
-    //function3
-
-
-    //function4
-
-
-    //function5
-
-
-    //function6
+        //Puntos
+        PUNpaises[i].puntos=
+        (PUNpaises[i].partidos.ganado*3)+ (PUNpaises[i].partidos.empatado*1);
     
+        //Rendimiento (performance)
+        PUNpaises[i].performance= (float)(PUNpaises[i].puntos/300)*100;  
+    }
 
 
+                    //iv) ORDENANDO SEGÚN PUNTOS MÁS ALTOS
+                    for(int i=0; i<cantidad-1; i++){
+                        for(int j=0; j<cantidad-1-i; j++){
+                            bool intercambiar= false;
+
+                            //mayor puntaje
+                            if(PUNpaises[j].puntos< PUNpaises[j+1].puntos){
+                                intercambiar= true;
+                            }else if(PUNpaises[j].puntos== PUNpaises[j+1].puntos){
+
+                                //por si hay empate --> mayor DG
+                                if(PUNpaises[j].diferenciaGoles< PUNpaises[j+1].diferenciaGoles){
+                                    intercambiar= true;
+                                }else if(PUNpaises[j].diferenciaGoles< PUNpaises[j+1].diferenciaGoles){
+                                    
+                                    //por si hay empate denuevo --> mayor GOL a favor
+                                    if(PUNpaises[j].goles.favor< PUNpaises[j+1].goles.favor){
+                                        intercambiar= true;
+                                    }
+                                }
+                            }
+
+                            //si alguno cumple
+                            if(intercambiar){
+                                team temp= PUNpaises[j];
+                                PUNpaises[j]= PUNpaises[j+1];
+                                PUNpaises[j+1]= temp;
+                            }
+                        }
+                    }
+
+
+    //v) TABLAAAAAAAAAAAAAAAAAAA
+    cout << "PAIS\t\tGANADO\t\tPERDIDO\t\tEMPATADO\tGOLES.FAVOR\tGOLES.CONTRA\tGOLES.DIFERENCIA\tPUNTOS\tPERFORMANCE\n";
+    cout << "--------------------------------------------------------------------------------------------------------------------------------------------------------------";
+    for(int i=0; i<cantidad; i++){
+        cout << "\n";
+        cout << PUNpaises[i].nombres << "\t\t" << PUNpaises[i].partidos.ganado << "\t\t" 
+        << PUNpaises[i].partidos.perdido << "\t\t" << PUNpaises[i].partidos.empatado << "\t\t" 
+        << PUNpaises[i].goles.favor << "\t\t" << PUNpaises[i].goles.contra << "\t\t" << PUNpaises[i].diferenciaGoles << "\t\t\t"
+        << PUNpaises[i].puntos << "\t" << PUNpaises[i].performance;
+    }
+
+
+                    //vi) EL CAMPEON --> como esta ordenado pues es directamente PUNpaises[0]
+                    cout << "\n\nCAMPEON: " << "| NOMBRE: " << PUNpaises[0].nombres  << " | PUNTOS: " << PUNpaises[0].puntos  << " | GOLES.DIFERENCIA: " << PUNpaises[0].diferenciaGoles  << " | PERFORMANCE: " << PUNpaises[0].performance << "%";
+
+
+    //
     cout << "\n\n-------------END\n";
     return 0;
 }
