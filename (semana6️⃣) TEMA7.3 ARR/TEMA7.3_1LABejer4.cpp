99e6fc851dc2d38878d9441318c99efb4a4a6eae
Arturo Yunior Torres Torre <arturo.torres.t@uni.pe>
2026-02-20 17:06:47 -0500

    --> 100% correcto (y todo hecho por mi)
    Solo me permiti ser guiado por algunas pequeñas cositas por GEMINI, pero toda la lógica de indices y lo demás fue todo mio.
    .
    Estoy orgulloso de esta rmejorando, definitivamente la practicaESCESIVA es lo que se necesita en este curso

--- (semana6️⃣) TEMA7.3 ARR/TEMA7.3_1LABejer4.cpp
+++ (semana6️⃣) TEMA7.3 ARR/TEMA7.3_1LABejer4.cpp
@@ -1,31 +1,55 @@
 #include <iostream>
 #include <fstream>
 #include <cstring>
+#include <cmath>    //for abs() integers
+#include <cstdlib>  //for fabs() floating point
 using namespace std;
 
 
-//
+//---0
 const char* nombreArchivo= "TEMA7.3_laguna.txt";
 
 
-//
-struct coo{
-    float x;
-    float y;
+//---C
+struct orden{
+    double v[3];
 };
 
 
-//
+//---B
+struct coo{
+    double x;
+    double y;
+};
+
+
+//---A
 struct laguna{
     int nt;
     int nv;
     //
-    coo coordenadasV[3];     
-    //apuntará a 3 de los 7 líneas de coorde. de vertices
-    int ordenT;       
-    //Dependiendo del orden de triangulo que sea: T1, T2, T3.... --> pues se eligirán las coordenadas que le corresponden
-    //va apuntar a las últimas "nt" lineas
+    coo* coordenadasV;     
+    //apuntará a --> las 7 líneas de coorde. de vertices
+    orden* ordenT;       
+    //apuntará a todas las líneas restantes "nt" --> lineas son
 };
+
+//--->1
+double area(laguna l){
+    double A=0;
+    for(int i=0; i<l.nt; i++){
+        double x[3], y[3];
+        for(int j=0; j<3; j++){     //para cada vertice
+            int indice= l.ordenT[i].v[j]-1;
+            x[j]= l.coordenadasV[indice].x;
+            y[j]= l.coordenadasV[indice].y;
+        }
+        //
+        A= A+ ( fabs( (x[0]*(y[2]-y[1]))+ (x[1]*(y[0]-y[2]))+ (x[2]*(y[1]-y[0]))) )/2.0;
+    }
+
+    return A;
+}
 
 
 //main
@@ -41,8 +65,40 @@ int main(){
     laguna l;
     leer >> l.nt;
     leer >> l.nv;
-    //aca quisiera adelantarme "nv" lineas y de ahí --> tomar como primer valor de "coordenadasV[3]" a la "nv+1" línea --> luego en otro bucle tomaría como valor al "nv+2" línea --> y así hasta alacanzar el valor de "nv+nt" línea
-    //solo que no sé cómo hacer eso en formator ".txt" --> en binary estoy muy seguro que solo tendría que usar "leer.seekg([nv-1]+1, ios::beg);" --> luego "leer.read((int*)(&p), sixeof(laguna));" cierto?, pero cómo hago esto en formato "txt."?
+    //
+    l.coordenadasV= new coo[l.nv];
+    for(int i=0; i<l.nv; i++){
+        leer >> l.coordenadasV[i].x;
+        leer >> l.coordenadasV[i].y;
+    }
+    //
+    l.ordenT= new orden[l.nt];
+    for(int i=0; i<l.nt; i++){
+        for(int j=0; j<3; j++){
+            leer >> l.ordenT[i].v[j];           //esta es una forma para poder trabajar con menos lineas
+        }
+    }
+    //4to
+    leer.close();
+    double areaTotal= area(l);
+
+
+    //1er, 2do
+    ofstream escribir("TEMA7.3_area.txt", ios::out);
+    if(!escribir){
+        cerr << "\n[ERROR]--> no se pudo abrir el archivo.\n";
+        return -1;
+    }
+    //3er
+    escribir << "El area total de las zonas trianguladas es: '" << areaTotal << "' m^2.\n";
+    //4to
+    escribir.close();
+
+
+    //deleting
+    delete[] l.ordenT;
+    delete[] l.coordenadasV;
+
 
     //
     cout << "\n\n-------------END\n";
